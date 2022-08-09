9d8e19777560484317b30edffc39f6419ead0d63
Starsii <estrellaanguiano99@gmail.com>
2022-08-09 14:14:16 -0500

    addingGameObjects

---
+++ .vscode/GameTrivia/ApexLegendsListOfQuestions.java
@@ -0,0 +1,16 @@
+public class ApexLegendsListOfQuestions {
+    
+    public ApexLegendsListOfQuestions () {
+        ApexLegend firstQuestion = new ApexLegend (
+            "The characters are known as?"
+            new List(KeyValuePair<Integer,string>{
+                new KeyValuePair<Integer, string>(0, "boosters");
+                new KeyValuePair<integer, string>(1, "mura");
+                new KeyValuePair<integer, string>(2, "legends");
+            }
+        );
+
+
+    }
+
+}
